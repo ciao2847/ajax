354f645db912fde15ad3b454b59cd7e7c7592776
CIAO2847 <a1188358@gmail.com>
2025-02-05 11:00:09 +0800

    非同步請求流程控制 callback promise Async/Await

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import ExchangeRate from "./ExchangeRate";
+import "bootstrap/dist/css/bootstrap.min.css"; // 引入 Bootstrap 的 CSS 檔案
+
+import "tailwindcss/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
